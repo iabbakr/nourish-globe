0570fbb6b51750456888fb1c3cc2a0715292e95d
Abubakar Ibrahim <abubakaribrahim603@gmail.com>
2024-12-09 18:03:01 +0100

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
-import Programs from './components/Programs';
+import Programs from './components/programs';
 import Impact from './components/Impact';
 import JoinUs from './components/JoinUs';
 import Blog from './components/Blog';
