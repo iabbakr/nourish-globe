32877a136faac4cad445177d74a594ad85b5e70a
Abubakar Ibrahim <abubakaribrahim603@gmail.com>
2024-12-11 08:43:10 +0100

    remove form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,7 @@ import React from 'react';
 const Contact = () => (
   <div className="contact">
     <h2>Contact Us</h2>
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Message:
-        <textarea name="message"></textarea>
-      </label>
-      <button type="submit">Send Message</button>
-    </form>
-
+    
     <section className="social-media">
       <h3>Follow Us</h3>
       <p>Facebook | Twitter | Instagram</p>
