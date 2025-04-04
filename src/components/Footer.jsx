bdd9b7342012f15c7eb20bf5f26d788db6c00c87
iabbakr <abubakaribrahim603@gmail.com>
2025-04-04 11:13:53 +0100

    added links on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ const Footer = () => (
   <footer className="footer">
     
     <nav>
-      <a href="#">Facebook</a>
-      <a href="#">Twitter</a>
-      <a href="#">Instagram</a>
+      <a href="https://www.facebook.com/profile.php?id=100093632378838&mibextid=kFxxJD&mibextid=kFxxJD">Facebook</a>
+      <a href="https://x.com/nourishglobe">X</a>
+      <a href="https://www.instagram.com/nourish_globe?igsh=bGgxcDlzZmZuZWxz&utm_source=qr">Instagram</a>
+      <a href="https://www.linkedin.com/company/nurtured-agro-resources-limited/">LinkedIn</a>
     </nav>
     <p>© 2024 Nourish Globe. All rights reserved.</p>
   </footer>
